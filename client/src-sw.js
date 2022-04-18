591e71881ffa841ec9a85130e7777aaec47bf499
Jamehzz <probableeme@gmail.com>
2022-04-17 20:07:00 -0400

    workaround for workbox bug

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -28,14 +28,11 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new CacheFirst({
+  new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
